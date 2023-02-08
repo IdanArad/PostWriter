7bd2958201c01318fabff65de3095ac7b281b39f
idan <idan@localhost.localdomain>
2023-02-08 09:41:21 +0200

    finished app

--- post-writer/src/App.js
+++ post-writer/src/App.js
@@ -7,6 +7,7 @@ import images from "./images";
 import { Box, Container, Grid, TextField, Button, Paper, Avatar } from "@mui/material";
 import AutorenewIcon from '@mui/icons-material/Autorenew';
 import { Configuration, OpenAIApi } from "openai";
+import { ThemeProvider, createTheme } from '@mui/system';
 import axios from "axios";
 
 function App() {
@@ -17,7 +18,12 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [response, setResponse] = useState("");
   const [selectedLogo, setSelectedLogo] = useState(defaultImage);
-  
+
+  const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
   useEffect(() => {
     try {
       chrome.storage.local.get(null, function (data) {
@@ -29,12 +35,6 @@ function App() {
       console.log("Error due to local state");
     }
   }, []);
-
-
-  const configuration = new Configuration({
-    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
 
   const getButtons = () => {
     return images.map((image) => {
@@ -55,10 +55,13 @@ function App() {
         builtprompt
       }})
       .then((response) => {
+        console.log(response.data);
         setResponse(response.data);
         setIsLoading(false);
       })
       .catch((error) => {
+        setResponse("Couldn't Reach to Chat GPT, Please try again");
+        setIsLoading(false);
         console.log(error);
       })
     } catch (e) {
@@ -68,67 +71,67 @@ function App() {
    }
    
   return (
-    <Container>
-      <Box sx={{ width: "77%", mt: 4  }}>
-        <Grid container>
-          <Grid item xs={12}>
-          <TextField
-            autoFocus
-            fullWidth
-            label="Tell me briefly what's happening, I'll generate for you..."
-            variant="outlined"
-            multiline
-            rows={4}
-            margin="normal"
-            value={prompt}
-            onChange={(e) => {
-              setPrompt(e.target.value);
-              chrome.storage.local.set({ prompt: e.target.value }); 
-            }}
-          />
-          <Box sx={{ width: "100%", mt: 2  }}>
-          {getButtons()} 
-          </Box>
-          <Box sx={{ width: "100%", mt: 4  }}
-          display="flex"
-          justifyContent="center"
-          alignItems="center">
-          <Button
-           disableElevation
-           variant="contained"
-           disabled={isLoading || selectedLogo.id == 0}
-           onClick={() => handleSubmit()}
-           startIcon={
-             isLoading && (
-               <AutorenewIcon
-                 sx={{
-                   animation: "spin 2s linear infinite",
-                   "@keyframes spin": {
-                     "0%": {
-                       transform: "rotate(360deg)",
-                     },
-                     "100%": {
-                       transform: "rotate(0deg)",
-                     },
-                   },
-                 }}
-               />
-             )
-           }
-          >
-            {selectedLogo.title}
-          </Button>
-          </Box>
-          
+        <Container>
+          <Box sx={{ width: "77%", mt: 4  }}>
+            <Grid container>
+              <Grid item xs={12}>
+              <TextField
+                autoFocus
+                fullWidth
+                label="Tell me briefly what's happening, I'll generate for you..."
+                variant="outlined"
+                multiline
+                rows={4}
+                margin="normal"
+                value={prompt}
+                onChange={(e) => {
+                  setPrompt(e.target.value);
+                  chrome.storage.local.set({ prompt: e.target.value }); 
+                }}
+              />
+              <Box sx={{ width: "100%", mt: 2  }}>
+              {getButtons()} 
+              </Box>
+              <Box sx={{ width: "100%", mt: 4  }}
+              display="flex"
+              justifyContent="center"
+              alignItems="center">
+              <Button
+              disableElevation
+              variant="contained"
+              disabled={isLoading || selectedLogo.id == 0}
+              onClick={() => handleSubmit()}
+              startIcon={
+                isLoading && (
+                  <AutorenewIcon
+                    sx={{
+                      animation: "spin 2s linear infinite",
+                      "@keyframes spin": {
+                        "0%": {
+                          transform: "rotate(360deg)",
+                        },
+                        "100%": {
+                          transform: "rotate(0deg)",
+                        },
+                      },
+                    }}
+                  />
+                )
+              }
+              >
+                {selectedLogo.title}
+              </Button>
+              </Box>
+              
+              </Grid>
+            </Grid>
+          <Grid item xs={12} sx={{mt:3, fontsize: 32 }}>
+          { response &&
+              <Paper  style={{whiteSpace: 'pre-line'}} sx={{ p: 4, color: 'success.main',fontSize:16 }}>{response}</Paper>
+          }
           </Grid>
-        </Grid>
-      <Grid item xs={12} sx={{mt:3}}>
-       { response &&
-          <Paper sx={{ p: 4  }}>{response}</Paper>
-       }
-      </Grid>
-      </Box>
-    </Container>
+          </Box>
+        </Container>
   );
 }
 
